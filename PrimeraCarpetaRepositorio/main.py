736037d1a0cc08fb348c50baf7f9d8f379429542
NicoBianchiL <nico.bianchi@uc.cl>
2025-08-28 10:33:47 -0400

    primera api

--- PrimeraCarpetaRepositorio/main.py
+++ PrimeraCarpetaRepositorio/main.py
@@ -1,4 +1,18 @@
-print("hello")
-x = "nico"
-print(x)
-%whos
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api/personal/profile")
+def profile():
+    return {
+        "name": "Tu Nombre",
+        "age": 21,
+        "height_cm": 181,
+        "weight_kg": 75.0,
+        "goal": {
+            "event": "Ironman 70.3 Valdivia",
+            "date": "2025-11-30",
+            "city": "Valdivia",
+            "country": "CL"
+        }
+    }
